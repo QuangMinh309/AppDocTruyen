4c0a111485680fb520b3c185c46b6b95d667c799
UIT.23520540 <23520540@gm.uit.edu.vn>
2025-06-11 09:42:41 +0700

    feat: admin screens (unfinished)

---
+++ frontend/app/src/main/java/com/example/frontend/ui/screen/admin/user_management.kt
@@ -0,0 +1,75 @@
+package com.example.frontend.ui.screen.admin
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.frontend.ui.components.ScreenFrame
+
+@Preview
+@Composable
+fun UserManagementScreen()
+{
+    ScreenFrame(
+        topBar = {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                // Back button
+                Button(
+//                    viewModel.onGoBack()
+                    onClick = {},
+                    colors =  ButtonDefaults.buttonColors(
+                        containerColor = Color.Transparent,
+                    ),
+                    contentPadding = PaddingValues(0.dp),
+                    modifier = Modifier
+                        .height(25.dp)
+                        .weight(0.33f)
+                        .wrapContentWidth(Alignment.Start)
+                ) {
+                    Text(
+                        text = "< Back",
+                        color = Color.White,
+                        style = TextStyle(
+                            fontSize = 16.sp
+                        )
+                    )
+                }
+
+                Text(
+                    text = "Category management",
+                    color = Color.White,
+                    style = TextStyle(
+                        fontSize = 16.sp,
+                        fontWeight = FontWeight.Bold
+                    ),
+                    modifier = Modifier
+                        .weight(0.5f)
+                        .wrapContentWidth(Alignment.CenterHorizontally)
+                        .align(Alignment.CenterVertically)
+                )
+                Spacer(modifier = Modifier.weight(0.33f))
+            }
+        }
+    )
+    {
+
+    }
+}
