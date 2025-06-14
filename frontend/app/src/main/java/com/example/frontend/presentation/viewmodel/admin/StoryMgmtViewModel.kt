8672996d74aaa3471f9485157ec6892c67edf57e
UIT.23520540 <23520540@gm.uit.edu.vn>
2025-06-14 23:41:50 +0700

    refactor: storymanagement

--- frontend/app/src/main/java/com/example/frontend/presentation/viewmodel/admin/StoryMgmtViewModel.kt
+++ frontend/app/src/main/java/com/example/frontend/presentation/viewmodel/admin/StoryMgmtViewModel.kt
@@ -1,11 +1,24 @@
 package com.example.frontend.presentation.viewmodel.admin
 
+import com.example.frontend.data.model.Story
 import com.example.frontend.presentation.viewmodel.BaseViewModel
 import com.example.frontend.services.navigation.NavigationManager
+import com.example.frontend.ui.screen.main_nav.ExampleList
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
 import javax.inject.Inject
 
 @HiltViewModel
 class StoryMgmtViewModel @Inject constructor(navigationManager: NavigationManager) : BaseViewModel(navigationManager) {
 
+    private val _selectedItem = MutableStateFlow<Story?>(null)
+    val selectedItem : MutableStateFlow<Story?> = _selectedItem
+
+    private val _stories = MutableStateFlow<List<Story>>(ExampleList)
+    val stories : MutableStateFlow<List<Story>> = _stories
+
+    fun openSettings()
+    {
+
+    }
 }
--- frontend/app/src/main/java/com/example/frontend/ui/screen/admin/story_management.kt
+++ frontend/app/src/main/java/com/example/frontend/ui/screen/admin/story_management.kt
@@ -1,32 +1,49 @@
 package com.example.frontend.ui.screen.admin
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.frontend.R
 import com.example.frontend.presentation.viewmodel.admin.StoryMgmtViewModel
 import com.example.frontend.services.navigation.NavigationManager
 import com.example.frontend.ui.components.ScreenFrame
+import androidx.compose.runtime.getValue
+import com.example.frontend.ui.components.StoryCard4
 
 @Composable
 fun StoryManagementScreen(viewModel: StoryMgmtViewModel = hiltViewModel())
 {
+    val stories by viewModel.stories.collectAsState()
     ScreenFrame(
         topBar = {
             Row(
@@ -71,7 +88,42 @@ fun StoryManagementScreen(viewModel: StoryMgmtViewModel = hiltViewModel())
         }
     )
     {
-
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .padding(top = 30.dp)
+        )
+        {
+            Text(
+                text = "Stories list",
+                color = Color.White,
+                style = TextStyle(
+                    fontSize = 30.sp,
+                    fontWeight = FontWeight.Bold,
+                    fontFamily = FontFamily(Font(R.font.reemkufifun_wght))
+                )
+            )
+            Spacer(modifier = Modifier.weight(1f))
+            Image(
+                painter = painterResource(R.drawable.setting_ic),
+                contentDescription = "Settings",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .size(50.dp)
+                    .clickable { viewModel.openSettings() }
+            )
+        }
+        Column(modifier = Modifier) {
+            Spacer(Modifier.height(20.dp))
+            LazyColumn (
+                verticalArrangement = Arrangement.spacedBy(16.dp),
+                contentPadding = PaddingValues(8.dp)
+            ){
+                items(stories) { story ->
+                    StoryCard4(story = story, onClick = {  })
+                }
+            }
+        }
     }
 }
 
